610dc74a72505d66a876a384df0512daacfb0782
quinn-skidmore <145470588+quinn-skidmore@users.noreply.github.com>
2024-04-30 15:30:06 +0000

    Finished

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 # Caesar Cipher Encrypt/Decrypt
 def caesar(message,shift):
-	pass
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	new_message = ""
+	message = message.upper()
+	for letter in message:
+		o_index = alphabet.index(letter)
+		o_index += shift
+		index = o_index % len(alphabet)
+		new_message += alphabet[index]
 
+	return new_message
 #Vigenere Encrypt
 def vigenere_encrypt(text,key):
-	pass
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	encrypted = ""
+	text = text.upper()
+	key = key.upper()
+	for i in range(0,len(text)):
+		t_index = alphabet.index(text[i])
+		k_index = alphabet.index(key[i % len(key)])
+		final_index = (t_index % len(alphabet) + k_index % len(alphabet)) % len(alphabet)
+		encrypted += alphabet[final_index]
+	return encrypted
+
 
 # Vigenere Decrypt
 def vigenere_decrypt(text, key):
-	pass
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	decrypted = ""
+	text = text.upper()
+	key = key.upper()
+	for i in range(0,len(text)):
+		t_index = alphabet.index(text[i])
+		k_index = alphabet.index(key[i % len(key)])
+		final_index = (t_index % len(alphabet) - k_index % len(alphabet)) % len(alphabet)
+		decrypted += alphabet[final_index]
+	return decrypted
+
